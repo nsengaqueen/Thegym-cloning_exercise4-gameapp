0ecfdcd0969c181c5af0c402c64a8457269a996f
queen <nsengaqueen123@gmail.com>
2024-05-16 19:10:33 +0200

    adding css

---
+++ dist/j.js
@@ -0,0 +1,18 @@
+function likes(names) {
+  // TODO
+  if(names.length<=0){
+    return `no one likes this`
+}if(names.length ==1){
+  return `${names} likes this`
+}
+  if(names.length ==2){
+  return `${names[0]} and ${names[1]} like this`
+}
+   if (names.length === 3) {
+    return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+    }
+ if(names.length ==4){
+  return `${names[0]}, ${names[1]} and 2 others   like this`
+}
+}  
+  
